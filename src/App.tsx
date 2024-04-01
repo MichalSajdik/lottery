11342b27be135730f4e5195c6854c50bcad2e257
Michal Šajdík <midas@Michal.local>
2024-04-01 10:34:06 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+let intervalId: number;
+let firstStop = true;
 
 function App() {
+
+    const [isSpinning, setIsSpinning] = useState(false);
+    const [currentSlotIndex, setCurrentSlotIndex] = useState(0);
+
+    // change above array to [{value: '3', image: 'https://media.gcflearnfree.org/ctassets/topics/246/share_flower_fullsize.jpg'}, ...  format
+    const lotterySlots = [
+        {value: '3', image: 'https://media.gcflearnfree.org/ctassets/topics/246/share_flower_fullsize.jpg'},
+        {value: '4', image: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg'},
+        {value: '5', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIT_X7uxU2yh1i7bMRYCjsmmUUEesmr3g0yEEEKxk-rg&s'},
+        {value: '6', image: 'https://th.bing.com/th/id/OIG.MxQxUggA0RKmKdTjwAqw'},
+        {value: '7a', image: 'https://www.codeproject.com/KB/GDI-plus/ImageProcessing2/grayscale.jpg'},
+        {value: '8a', image: 'https://media.gcflearnfree.org/ctassets/topics/246/share_flower_fullsize.jpg'}
+        ]
+
+    useEffect(() => {
+
+        if (isSpinning) {
+            intervalId = window.setInterval(()=>{
+                setCurrentSlotIndex((prevIndex) => (prevIndex + 1) % lotterySlots.length);
+            }, 100); // Adjust the interval as needed
+        } else {
+            clearInterval(intervalId);
+        }
+
+        return () => clearInterval(intervalId);
+    }, [isSpinning]);
+
+    const handleStart = () => {
+        setIsSpinning(true);
+    };
+
+    const handleStop = () => {
+        setIsSpinning(false);
+        if(firstStop) {
+            firstStop = false;
+            clearInterval(intervalId);
+            setCurrentSlotIndex(2);
+        }
+    };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+
+          <div>
+              <button onClick={handleStart} disabled={isSpinning}>
+                  Start
+              </button>
+              <button onClick={handleStop} disabled={!isSpinning}>
+                  Stop
+              </button>
+          </div>
+          <div style={{backgroundColor: "#ffd",
+              fontSize: "8em"}}>
+              {lotterySlots[currentSlotIndex].value}
+              <br/>
+              <img src={lotterySlots[currentSlotIndex].image} alt="logo"
+                   style={{ maxWidth: '400px', maxHeight: '400px' }} />
+          </div>
+
+
+      </div>
   );
 }
 
